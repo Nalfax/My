098fe269b0683a6b679e92ea439be9cd1edf03e5
unknown <tom.rizo31@gmail.com>
2020-08-17 02:33:56 +0200

    debut des placement

--- app.js
+++ app.js
@@ -50,12 +50,12 @@ window.onscroll = function () {
 
   if (document.documentElement.scrollTop > 80) {
       document.getElementById("navbar").style.background = "black";
-      document.getElementById("navbar").style.padding = "7px 200px";
-      document.getElementById("logo").style.color = "white";
+      document.getElementById("navbar").style.padding = "7px 40px";
+      document.getElementById("background").style.color = "white";
   }
   else {
       document.getElementById("navbar").style.background = "white";
       document.getElementById("navbar").style.padding = "12px 40px";   
-      document.getElementById("logo").style.color = "black";  
+      document.getElementById("background").style.color = "black";  
   }
 }
